0d44ca04dffc7868bc114b58aa891e3d574a7ce6
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-03-15 14:03:22 +0300

    update

--- app/screens/AudioList.jsx
+++ app/screens/AudioList.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import {View, StyleSheet, Text, ScrollView} from 'react-native';
-import { AudioContext } from '../context/AudioProvider';
+import React, { Component } from "react";
+import { View, Text, Dimensions } from "react-native";
+import { AudioContext } from "../context/AudioProvider";
+import { RecyclerListView, LayoutProvider } from "recyclerlistview";
 
 class AudioList extends Component {
-    static contextType = AudioContext
-    render() {
-        return (
-            <ScrollView>
-                {this.context.audioFiles.map(item => 
-                <Text style={{
-                    padding: 10, 
-                    borderBottomColor: 'red', 
-                    borderBottomWidth: 2,
-                }} 
-                key={item.id}
-                >
-                {item.filename}
-                </Text>)}
-                
-            </ScrollView>
-        );
+  static contextType = AudioContext;
+
+  layoutProvider = new LayoutProvider(
+    () => "audio",
+    (_, dim) => {
+      dim.width = Dimensions.get("window").width;
+      dim.height = 70;
     }
+  );
+
+  rowRenderer = (_, item) => {
+    return <Text>{item.filename}</Text>;
+  };
+
+  render() {
+    const { dataProvider } = this.context;
+    return (
+      <View style={{ flex: 1 }}>
+        <RecyclerListView
+          dataProvider={dataProvider}
+          layoutProvider={this.layoutProvider}
+          rowRenderer={this.rowRenderer}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
-
 export default AudioList;
-
