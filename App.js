3e0292277b67b8b9b041749f7fb10ded91747868
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-03-17 09:33:22 +0300

    Play Pause and Resume Audio

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Text, View, Button, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AudioProvider from "./app/context/AudioProvider";
+import AudioListItem from "./app/components/AudioListItem";
 
 export default function App() {
   return (
--- app/screens/AudioList.jsx
+++ app/screens/AudioList.jsx
@@ -2,32 +2,120 @@ import React, { Component } from "react";
 import { View, Text, Dimensions } from "react-native";
 import { AudioContext } from "../context/AudioProvider";
 import { RecyclerListView, LayoutProvider } from "recyclerlistview";
+import AudioListItem from "../components/AudioListItem";
+import Screen from "../components/Screen";
+import OptionModal from "../components/OptionModal";
+import { Audio } from "expo-av";
 
 class AudioList extends Component {
   static contextType = AudioContext;
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      optionModalVisible: false,
+      playbackObj: null,
+      soundObj: null,
+      currentAudio: {},
+    };
+
+    this.currentItem = {};
+  }
+
   layoutProvider = new LayoutProvider(
-    () => "audio",
-    (_, dim) => {
-      dim.width = Dimensions.get("window").width;
-      dim.height = 70;
+    (i) => "audio",
+    (type, dim) => {
+      switch (type) {
+        case "audio":
+          dim.width = Dimensions.get("window").width;
+          dim.height = 70;
+          break;
+        default:
+          dim.width = 0;
+          dim.height = 0;
+      }
     }
   );
 
-  rowRenderer = (_, item) => {
-    return <Text>{item.filename}</Text>;
+  handleAudioPress = async (audio) => {
+    // playing audio for the first time.
+    if (this.state.soundObj === null) {
+      const playbackObj = new Audio.Sound();
+      const status = await playbackObj.loadAsync(
+        { uri: audio.uri },
+        { shouldPlay: true }
+      );
+      return this.setState({
+        ...this.state,
+        currentAudio: audio,
+        playbackObj: playbackObj,
+        soundObj: status,
+      });
+    }
+
+    // pause audio
+    if (this.state.soundObj.isLoaded && this.state.soundObj.isPlaying) {
+      const status = await this.state.playbackObj.setStatusAsync({
+        shouldPlay: false,
+      });
+      return this.setState({
+        ...this.state,
+        soundObj: status,
+      });
+    }
+
+    // resume audio
+    if (
+      this.state.soundObj.isLoaded &&
+      !this.state.soundObj.isPlaying &&
+      this.state.currentAudio.id === audio.id
+    ) {
+      const status = await this.state.playbackObj.playAsync();
+      return this.setState({
+        ...this.state,
+        soundObj: status,
+      });
+    }
+  };
+
+  rowRenderer = (type, item) => {
+    return (
+      <AudioListItem
+        title={item.filename}
+        duration={item.duration}
+        onAudioPress={() => this.handleAudioPress(item)}
+        onOptionPress={() => {
+          this.currentItem = item;
+          this.setState({ ...this.state, optionModalVisible: true });
+        }}
+      />
+    );
   };
 
   render() {
-    const { dataProvider } = this.context;
     return (
-      <View style={{ flex: 1 }}>
-        <RecyclerListView
-          dataProvider={dataProvider}
-          layoutProvider={this.layoutProvider}
-          rowRenderer={this.rowRenderer}
-        />
-      </View>
+      <AudioContext.Consumer>
+        {({ dataProvider }) => {
+          return (
+            <Screen>
+              <RecyclerListView
+                dataProvider={dataProvider}
+                layoutProvider={this.layoutProvider}
+                rowRenderer={this.rowRenderer}
+              />
+              <OptionModal
+                onPlayPress={() => console.log("Playing audio")}
+                onPlayListPress={() => console.log("Adding to the playlist")}
+                currentItem={this.currentItem}
+                onclose={() =>
+                  this.setState({ ...this.state, optionModalVisible: false })
+                }
+                visible={this.state.optionModalVisible}
+              />
+            </Screen>
+          );
+        }}
+      </AudioContext.Consumer>
     );
   }
 }
