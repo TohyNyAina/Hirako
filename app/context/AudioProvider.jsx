d6fd3d399c34ed911107a6e4a901b2a55cb43784
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-03-23 16:44:52 +0300

    pause in player ui

--- app/context/AudioProvider.jsx
+++ app/context/AudioProvider.jsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Alert } from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import { DataProvider } from "recyclerlistview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Audio } from 'expo-av';
 
 export const AudioContext = createContext();
 
@@ -103,6 +104,9 @@ class AudioProvider extends Component {
 
   componentDidMount() {
     this.getPermission();
+    if(this.state.playbackObj === null){
+      this.setState({...this.state, playbackObj: new Audio.Sound()})
+    }
   }
 
   updateState = (prevState, newState = {}) => {
