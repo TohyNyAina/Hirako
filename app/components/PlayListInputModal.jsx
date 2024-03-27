403bf43cb6df5790012768d57a45aedc5fb77ec2
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-03-27 11:28:38 +0300

    playlist

---
+++ app/components/PlayListInputModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet, Modal} from 'react-native';
+
+const PlayListInputModal = () => {
+    return (
+        <Modal></Modal>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default PlayListInputModal;
